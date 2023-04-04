74c16dbb8008a926ec1a2a1ccc5abc954829e52a
sujeong1201 <37768793+sujeong1201@users.noreply.github.com>
2023-04-04 10:08:21 +0900

    Update 로또의 최고 순위와 최저 순위.java

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
         int[] answer = new int[2];
-        int[] rank = {6, 6, 5, 4, 3, 2, 1};
+        int[] rank = {6, 6, 5, 4, 3, 2, 1};  // 등수 매칭
         
-        int same = 0;
-        int unknown = 0;
+        int same = 0;  // 일치하는 번호의 개수
+        int unknown = 0;  // 알아볼 수 없는 수의 개수
         for(int i=0; i<lottos.length; i++) {
         	if(lottos[i] == 0) {
         		unknown++;
@@ -19,7 +19,9 @@ class Solution {
         	}
         }
         
+        // 최고 순위는 unknown 수가 모두 일치하는 경우
         answer[0] = rank[same + unknown];
+        // 최저 순위는 unknown 수가 모두 일치하지 않는 경우
         answer[1] = rank[same];
         
         return answer;
