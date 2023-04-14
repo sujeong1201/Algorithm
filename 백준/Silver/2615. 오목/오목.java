32ee7ca14def8e2016461778f939607153d84616
sujeong1201 <37768793+sujeong1201@users.noreply.github.com>
2023-04-14 11:56:02 +0900

    [Silver I] Title: 오목, Time: 156 ms, Memory: 16368 KB -BaekjoonHub

---
+++ 백준/Silver/2615. 오목/오목.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[][] map = new int[19][19];
+	static int[] dr = {1, 1, 0, -1};
+	static int[] dc = {0, 1, 1, 1};
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		for(int i=0; i<19; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j=0; j<19; j++) map[i][j] = Integer.parseInt(st.nextToken());
+		}
+		
+		boolean blackWin = false;
+		boolean whiteWin = false;
+		int r = 0, c = 0;
+		for(int i=0; i<19; i++) {
+			for(int j=0; j<19; j++) {
+				if(map[j][i] != 0 && omok(j, i)) {
+					if(map[j][i] == 1) blackWin = true;
+					else whiteWin = true;
+					
+					r = j; 
+					c = i;
+				}
+			}
+		}
+		
+		if(blackWin) System.out.print(1 + "\n" + (r + 1) + " " + (c + 1));
+		else if(whiteWin) System.out.println(2 + "\n" + (r + 1) + " " + (c + 1));
+		else System.out.println(0);
+	}
+
+	private static boolean omok(int r, int c) {
+		int color = map[r][c];
+		
+		for(int d=0; d<4; d++) {
+			int nr = r;
+			int nc = c;
+			int cnt = 1;
+			while(true) {
+				nr = nr + dr[d];
+				nc = nc + dc[d];
+				if(nr < 0 || nr >= 19 || nc < 0 || nc >= 19) break;
+				if(map[nr][nc] != color) break;
+				
+				cnt++;
+				// 한 지점에서 오목과 육목이 겹쳐 있을 수 있기 때문에  바로 리턴하면 안됨
+				if(cnt >= 6) break;
+			}
+			
+			if(cnt == 5) {
+				nr = r - dr[d];
+				nc = c - dc[d];
+				if((nr >= 0 && nr < 19 && nc >= 0 && nc < 19 && map[nr][nc] != color)
+						|| (nr < 0 || nr >= 19 || nc < 0 || nc >= 19)) {
+					return true;
+				}
+			}
+		}
+		
+		return false;
+	}
+}
