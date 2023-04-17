fd8a8317307bc9a4629945769722db79d38d4988
sujeong1201 <37768793+sujeong1201@users.noreply.github.com>
2023-04-17 22:05:55 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 368 ms, Memory: 23356 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -1,37 +1,25 @@
 import java.util.Scanner;
-import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String str = sc.nextLine();
 		
-		Stack<Character> output = new Stack<>();
+		StringTokenizer st = new StringTokenizer(str, "<> ", true);
 		boolean isTag = false;
-		String answer = "";
-		
-		for(int i = 0; i < str.length(); i++) {
-			char ch = str.charAt(i);
-			
+		StringBuffer answer = new StringBuffer();
+		while(st.hasMoreTokens()) {
+			String token = st.nextToken();
 			if(!isTag) {
-				if(ch == ' ' || ch == '<') {
-					while(!output.isEmpty()) {
-						answer += output.pop();
-					}
-					answer += ch;
-					if(ch == '<') isTag = true;
-				} else {
-					output.push(ch);
-				}
+				answer.append(new StringBuffer(token).reverse());
+				if("<".equals(token)) isTag = true;
 			} else {
-				answer += ch;
-				if(ch == '>') isTag = false;
+				answer.append(token);
+				if(">".equals(token)) isTag = false;
 			}
 		}
 		
-		while(!output.isEmpty()) {
-			answer += output.pop();
-		}
-		System.out.println(answer);
+		System.out.println(answer.toString());
 	}
 }
