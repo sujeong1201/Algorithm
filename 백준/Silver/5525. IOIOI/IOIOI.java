095944ba2cc78fe2bb765a5f6b72828e410bddd3
정수정 <37768793+sujeong1201@users.noreply.github.com>
2024-01-20 16:38:09 +0900

    [Silver I] Title: IOIOI, Time: 264 ms, Memory: 20052 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -3,23 +3,31 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int S = Integer.parseInt(br.readLine());
-		String str = br.readLine();
-		
-		String p = "";
-		for(int i=0; i<N; i++) {
-			if(i == N - 1) p += "IOI";
-			else p += "IO";
-		}
-	
-		int cnt = 0;
-		for(int i=0; i<S-2*N; i++) {
-			String subStr = str.substring(i, i + 2 * N + 1);
-			if(subStr.equals(p)) ++cnt;
-		}
-		System.out.println(cnt);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+
+        int answer = 0;
+        int index = 0;
+        for(; index < M; index++) {
+            if(str.charAt(index) == 'O') continue;
+            
+            int no = 0;
+            while(true) {
+                if(index + 1 < M && str.charAt(index + 1) == 'O' &&
+                        index + 2 < M && str.charAt(index + 2) == 'I') {
+                    no++;
+                    index += 2;
+                } else {
+                    break;
+                }
+            }
+
+            if(no >= N) answer += (no - N + 1);
+        }
+
+        System.out.println(answer);
+    }
 }
