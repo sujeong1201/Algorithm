a7dc3653a12b40d758010d20a3206d43b80bb3db
정수정 <37768793+sujeong1201@users.noreply.github.com>
2024-03-29 23:10:56 +0900

    [Silver III] Title: 타노스, Time: 216 ms, Memory: 17772 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.java
+++ 백준/Silver/20310. 타노스/타노스.java
@@ -3,18 +3,38 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    String s = sc.nextLine();
-    char[] chArr = s.toCharArray();
+    String str = sc.nextLine();
+    int length = str.length();
+
     int oneCnt = 0;
     int zeroCnt = 0;
-    for (int i = 0; i < chArr.length; i++) {
-      if (chArr[i] == '0') zeroCnt++;
+    for (int i = 0; i < length; i++) {
+      if (str.charAt(i) == '0') zeroCnt++;
       else oneCnt++;
     }
 
+    oneCnt /= 2;
+    zeroCnt /= 2;
+    boolean[] check = new boolean[length];
+    for (int i = 0; i < length; i++) {
+      if (str.charAt(i) == '0') {
+        check[i] = true;
+        zeroCnt--;
+        if (zeroCnt == 0) break;
+      }
+    }
+    for (int i = length - 1; i >= 0; i--) {
+      if (str.charAt(i) == '1') {
+        check[i] = true;
+        oneCnt--;
+        if (oneCnt == 0) break;
+      }
+    }
+
     StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < zeroCnt / 2; i++) sb.append('0');
-    for (int i = 0; i < oneCnt / 2; i++) sb.append('1');
+    for (int i = 0; i < length; i++) {
+      if (check[i]) sb.append(str.charAt(i));
+    }
     System.out.println(sb);
   }
 }
