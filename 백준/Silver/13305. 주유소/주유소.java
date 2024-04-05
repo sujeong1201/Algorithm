811d5e2061790b3c8cd90eeb8dbfe5b1a7b169c5
정수정 <37768793+sujeong1201@users.noreply.github.com>
2024-04-05 16:32:07 +0900

    [Silver III] Title: 주유소, Time: 472 ms, Memory: 47672 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -14,19 +15,19 @@ public class Main {
     st = new StringTokenizer(br.readLine());
     for (int i = 0; i < N; i++) prices[i] = Integer.parseInt(st.nextToken());
 
-    int answer = 0;
+    BigInteger answer = new BigInteger("0");
     int prevPrice = prices[0];
-    int distanceSum = distances[0];
+    long distanceSum = distances[0];
     for (int i = 1; i < N - 1; i++) {
       if (prices[i] < prevPrice) {
-        answer += prevPrice * distanceSum;
+        answer = answer.add(BigInteger.valueOf(prevPrice * distanceSum));
         prevPrice = prices[i];
         distanceSum = 0;
       }
 
       distanceSum += distances[i];
     }
-    answer += prevPrice * distanceSum;
+    answer = answer.add(BigInteger.valueOf(prevPrice * distanceSum));
 
     System.out.println(answer);
   }
