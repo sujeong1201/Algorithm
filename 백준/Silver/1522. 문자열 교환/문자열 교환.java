dcf8724bb6f963a66faf797a235b073fd73d25ae
정수정 <37768793+sujeong1201@users.noreply.github.com>
2024-04-24 12:06:27 +0900

    [Silver I] Title: 문자열 교환, Time: 124 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/1522. 문자열 교환/문자열 교환.java
+++ 백준/Silver/1522. 문자열 교환/문자열 교환.java
@@ -1,9 +1,11 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    String str = sc.nextLine();
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String str = br.readLine();
     int strLength = str.length();
     int aCnt = 0;
     for (int i = 0; i < strLength; i++) {
